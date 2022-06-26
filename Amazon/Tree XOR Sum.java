14579f68bc54e4be678522561e87fbcfb83bd744
Ruiyue Wang <phynew0111@gmail.com>
2022-06-26 13:58:54 -0400

    Update Tree XOR Sum.java

--- Amazon/Tree XOR Sum.java
+++ Amazon/Tree XOR Sum.java
@@ -114,6 +114,7 @@ public class xortree {
         }
         return ans;
     }
+  
     public static void main(String args[]) {
         System.out.println(solve(4, new int[][]{{1,2},{2,3},{4,1}}));
         System.out.println(solve(5, new int[][]{{1,2},{1,4},{1,5},{3,4}}));
